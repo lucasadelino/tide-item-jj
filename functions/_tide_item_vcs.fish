2eff634fc27d3591f6580d3a9d40055ad3852276
Lucas Adelino <lcsadelino@gmail.com>
2026-09-01 12:16:23 -0400

    fix(vcs): set _tide_location_color
    This replicates Tide's caching of `_tide_location_color`, which it only
    does if the `git` item is present.

--- functions/_tide_item_vcs.fish
+++ functions/_tide_item_vcs.fish
@@ -1,4 +1,9 @@
 function _tide_item_vcs
+    # Cache _tide_location_color so that the git branch/tag/ref name prints
+    if not set -q _tide_location_color
+        set_color $tide_git_color_branch | read -gx _tide_location_color
+    end
+
     # Are we in a JJ repo?
     if test $PWD != $HOME
         and jj root &>/dev/null
