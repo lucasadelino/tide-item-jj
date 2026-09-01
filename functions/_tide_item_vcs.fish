4f2c65430668b07f3f1913ce716ee90a1de8a95d
Lucas Adelino <lcsadelino@gmail.com>
2026-09-01 16:08:12 -0400

    refactor(vcs): remove redundant test calls

--- functions/_tide_item_vcs.fish
+++ functions/_tide_item_vcs.fish
@@ -7,12 +7,6 @@ function _tide_item_vcs
     # Are we in a JJ repo?
     if test $PWD != $HOME
         and jj root &>/dev/null
-        set in_jjdir true
-    else
-        set in_jjdir false
-    end
-
-    if test "$in_jjdir" = true
         _tide_item_jj
     else
         _tide_item_git
